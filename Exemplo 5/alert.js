87108624d6cedf99098037bd4759503c3f98fabb
Jonathan <38326991+jonathanamorim@users.noreply.github.com>
2018-08-01 15:31:47 -0300

    Add files via upload

---
+++ Exemplo 5/alert.js
@@ -0,0 +1,13 @@
+function Aluno(Nome){
+    this.nome = Nome;
+}
+function Professor(Nome) {
+    this.nome = Nome;
+}
+function Turma(Aluno, Professor){
+    this.aluno = Aluno;
+    this.professor = Professor;                
+}
+function Imprimir(){
+    alert();
+}
